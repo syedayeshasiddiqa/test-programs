d0c22ccf566ef258127ed7d5d36a2f5cd4027d22
syedayeshasiddiqa <syedayesha0829@gmail.com>
2022-05-17 20:35:25 +0530

    my first commit

---
+++ Test1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Test1{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m=sc.nextInt();
+int a[][]=new int[n][m];
+int b[][]=new int[n][m];
+int c[][]=new int[n][m];
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<m;j++)
+{
+a[i][j]=sc.nextInt();
+}
+}
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<m;j++)
+{
+b[i][j]=sc.nextInt();
+}
+}
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<m;j++)
+{
+c[i][j]=a[i][j]+b[i][j];
+}
+}
+ 
+
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<m;j++)
+{
+System.out.println("c["+i+"]["+j+"] = "+c[i][j]);
+}
+}
+System.out.println("This is 2D Array ");
+}
+}
