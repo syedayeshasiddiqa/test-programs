ed49d8db60fcaf34f93ae12b0961c395bce1a28f
syedayeshasiddiqa <93445468+syedayeshasiddiqa@users.noreply.github.com>
2023-12-07 17:06:44 +0530

    Update Details.java

--- Details.java
+++ Details.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 public class Program{
-int rollno;
-String studentname;
-String university;
-Program(){
-Scanner sc=new Scanner(System.in);
-rollno rn=sc.nextInt();
-sc.nestLine();
-studentname std=sc.nextLine();
-university un=sc.nextLine();
-}
-public static void main(String[] args){
-Program dt=new Program();
-System.out.println(dt.std);
-System.out.println(dt.rn);
-System.out.println(dt.un);
-}
+    int rollNo;
+    String studentName;
+    String university;
+    Program(){
+        Scanner sc = new Scanner(System.in);
+        rollNo = sc.nextInt();
+        sc.nextLine();
+        studentName = sc.nextLine();
+        university = sc.nextLine();
+    }
+    public static void main(String[] args){
+        Program pgm = new Program();
+        System.out.println(pgm.rollNo);
+        System.out.println(pgm.studentName);
+        System.out.println(pgm.university);
+    }
 }
 
 
