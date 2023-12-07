3fab51ffe40a448fb9c3355ee63cbbf96b01408b
syedayeshasiddiqa <93445468+syedayeshasiddiqa@users.noreply.github.com>
2023-12-07 17:30:21 +0530

    Update Test3.java

--- Test3.java
+++ Test3.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 public class Test3{
-public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
-//int m=sc.nextInt();
-for(int i=0;i<n;i++){
-for(int j=1;j<=i;j++){
-System.out.print(j);
-}
-System.out.println();
-}
-}
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=i+1;j++){
+                System.out.print(j);
+            }
+             System.out.println();
+        }
+    }
 }
