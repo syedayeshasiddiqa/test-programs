d8ed13782a05f01ee6e19ac125dc1c1346792c17
syedayeshasiddiqa <93445468+syedayeshasiddiqa@users.noreply.github.com>
2023-12-07 17:36:45 +0530

    Update Testeg.java

--- Testeg.java
+++ Testeg.java
@@ -1,18 +1,20 @@
 import java.util.*;
 public class Details{
-String Studentname;
-int rollno;
-String university;
-Details(){
-Scanner sc=new Scanner(System.in);
-Studentname std=sc.nextLine();
-rollno rn=sc.nextInt();
-university un=sc.nextLine();
+    String studentName;
+    int rollNo;
+    String university;
+    public void details(){
+        Scanner sc = new Scanner(System.in);
+        studentName = sc.nextLine();
+        rollNo = sc.nextInt();
+        sc.nextLine();
+        university = sc.nextLine();
+        System.out.println(studentName);
+        System.out.println(rollNo );
+        System.out.println(university);
+   }
+    public static void main(String[] args){
+       Details dt=new Details();
+       dt.details();
+    }
 }
-public static void main(String[] args){
-Details dt=new Details;
-dt.Details();
-}
-}
-
-
